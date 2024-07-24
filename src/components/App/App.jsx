7f34cd660afceff10554235a56274be9a3f1d942
OksanaGukova <kseniagukova@ukr.net>
2024-07-24 22:18:15 +0300

    rename

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { fetchContacts } from "../../redux/operations";
+import { fetchContacts } from "../../redux/contactsOps";
 import ContactForm
   from "../ContactForm/ContactForm";
 import SearchBox from '../SearchBox/SearchBox'
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,5 +1,5 @@
 import { createSelector, createSlice } from "@reduxjs/toolkit";
-import { addContact, deleteContact, fetchContacts } from "./operations";
+import { addContact, deleteContact, fetchContacts } from "./contactsOps";
 
 const contactsSlice = createSlice({
   name: "contacts",
